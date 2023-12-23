a0bce343d16b03bfc5be86175009300413ab1240
ÖMER KINCAL <202804010@ogr.cbu.edu.tr>
2023-12-23 18:18:56 +0300

    first commit

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,7 @@
+
+7
+HELP.md,1\a\1a273f73b7a477b0745593641bd708a6182c7ad5
+B
+docker-compose.yml,3\5\35b8c13cf2eb2a194eada000eb310d65aed53b2a
+x
+Hsrc/main/java/org/omarkincal/chatapp/library/config/WebSocketConfig.java,9\0\900004d788fcede74b07f46f6054d6ff8ec1916e
---
+++ src/main/java/org/omarkincal/chatapp/library/config/WebSocketConfig.java
@@ -0,0 +1,11 @@
+package org.omarkincal.chatapp.library.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+}
